1283e9c8f6bac45d46c0ff2dfc9e0af50e7b4f85
Jeewon Heo <jeewon.heo@motherduck.com>
2024-12-14 23:23:03 +0100

    idk what i'm doing

---
+++ src/Day01.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	day := 01
+
+	fmt.Printf("Day %02d\n", day)
+
+}
---
+++ src/utils/parser.go
@@ -0,0 +1,10 @@
+package utils
+
+type Parser struct {
+	Filename string
+	ColNums  int
+}
+
+func (parser Parser) ParseInput() {
+	parser.Filename
+}
