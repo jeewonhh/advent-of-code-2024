947aec109b024eb10a64530cff200a690ef7d35e
Jeewon Heo <jeewon.heo@motherduck.com>
2024-12-18 23:49:50 +0100

    yay! day 03 done!

--- GO/src/main/Day03.go
+++ GO/src/main/Day03.go
@@ -41,3 +41,44 @@ func (d Day03) Part01(memory string) int {
 	}
 	return answer
 }
+
+func (d Day03) Part02(memory string) int {
+	i, j := 0, 0
+	answer := 0
+	enabled := true
+	for i < len(memory)-4 {
+		if memory[i:i+4] == "do()" {
+			enabled = true
+		}
+		if i < len(memory)-7 && memory[i:i+7] == "don't()" {
+			enabled = false
+		}
+		if enabled && memory[i:i+4] == "mul(" {
+			start := i + 4
+			j = start
+			for j < start+3 && unicode.IsDigit(rune(memory[j])) {
+				j++
+			}
+			if (j-start) < 1 || memory[j] != ',' {
+				i = j
+				continue
+			}
+			a, _ := strconv.Atoi(memory[start:j])
+			start = j + 1
+			j = start
+			for j < start+3 && unicode.IsDigit(rune(memory[j])) {
+				j++
+			}
+			if (j-start) < 1 || memory[j] != ')' {
+				i = j
+				continue
+			}
+			b, _ := strconv.Atoi(memory[start:j])
+			answer += a * b
+			i = j
+		} else {
+			i++
+		}
+	}
+	return answer
+}
