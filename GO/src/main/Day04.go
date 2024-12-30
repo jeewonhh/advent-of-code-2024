2e5d7b9a8c3e991834cfbf8ef499461cfe967b87
Jeewon Heo <jeewon.heo@motherduck.com>
2024-12-30 13:34:07 +0100

    day 04 part 2

--- GO/src/main/Day04.go
+++ GO/src/main/Day04.go
@@ -12,6 +12,10 @@ const XMAS = "XMAS"
 const SAMX = "SAMX"
 const LEN = 4
 
+const MAS = "MAS"
+const SAM = "SAM"
+const MASLEN = 3
+
 func lookEverywhere(puzzle []string, row int, col int) int {
 	count := 0
 	rows := len(puzzle)
@@ -61,11 +65,49 @@ func lookEverywhere(puzzle []string, row int, col int) int {
 	return count
 }
 
+func lookAtBox(puzzle []string, row int, col int) int {
+	rows := len(puzzle)
+	cols := len(puzzle[0])
+
+	if row > rows-MASLEN || col > cols-MASLEN {
+		return 0
+	}
+
+	var sb strings.Builder
+	for i := 0; i < MASLEN; i++ {
+		sb.WriteRune(rune(puzzle[row+i][col+i]))
+	}
+	first := sb.String()
+	if first != MAS && first != SAM {
+		return 0
+	}
+	sb.Reset()
+	for i := 0; i < MASLEN; i++ {
+		sb.WriteRune(rune(puzzle[row+MASLEN-i-1][col+i]))
+	}
+	second := sb.String()
+	if second != MAS && second != SAM {
+		return 0
+	}
+	return 1
+}
+
 func (d Day04) Part01(puzzle []string) int {
 	count := 0
 	for i := 0; i < len(puzzle); i++ {
 		for j := 0; j < len(puzzle[0]); j++ {
 			count += lookEverywhere(puzzle, i, j)
+		}
+	}
+	return count
+}
+
+func (d Day04) Part02(puzzle []string) int {
+	count := 0
+
+	for i := 0; i < len(puzzle); i++ {
+		for j := 0; j < len(puzzle[0]); j++ {
+			count += lookAtBox(puzzle, i, j)
 		}
 	}
 	return count
--- GO/src/test/Day04_test.go
+++ GO/src/test/Day04_test.go
@@ -50,28 +50,39 @@ func (tester Day04Tester) TestPart01(t *testing.T) {
 	}
 }
 
-//func (tester Day04Tester) TestPart02Mock(t *testing.T) {
-//	answer := tester.solve.Part02(tester.records)
-//	want := 4
-//	if answer != want {
-//		t.Errorf("got %d, want %d", answer, want)
-//	}
-//}
-//
-//func (tester Day04Tester) TestPart02(t *testing.T) {
-//	records := utils.Parse2DInts(tester.input_file)
-//	answer := tester.solve.Part02(records)
-//	want := 301
-//	if answer != want {
-//		t.Errorf("got %d, want %d", answer, want)
-//	}
-//}
+func (tester Day04Tester) TestPart02Mock(t *testing.T) {
+	//.M.S......
+	//..A..MSMS.
+	//.M.S.MAA..
+	//..A.ASMSM.
+	//.M.S.M....
+	//..........
+	//S.S.S.S.S.
+	//.A.A.A.A..
+	//M.M.M.M.M.
+	//..........
+	answer := tester.solve.Part02(tester.puzzle)
+	want := 9
+	if answer != want {
+		t.Errorf("got %d, want %d", answer, want)
+	}
+}
+
+func (tester Day04Tester) TestPart02(t *testing.T) {
+	bytes, _ := os.ReadFile(tester.input_file)
+	puzzle := strings.Split(string(bytes), "\n")
+	answer := tester.solve.Part02(puzzle)
+	want := 0
+	if answer != want {
+		t.Errorf("got %d, want %d", answer, want)
+	}
+}
 
 func TestDay04(t *testing.T) {
 	tester := NewDay04Tester(2024, 04)
 
 	t.Run("Part1 (Mock)", tester.TestPart01Mock)
 	t.Run("Part1", tester.TestPart01)
-	//t.Run("Part2 (Mock)", tester.TestPart02Mock)
-	//t.Run("Part2", tester.TestPart02)
+	t.Run("Part2 (Mock)", tester.TestPart02Mock)
+	t.Run("Part2", tester.TestPart02)
 }
